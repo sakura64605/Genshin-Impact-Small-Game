26669d1191e522f921d65771305fcdde8e506cc6
DengHongJie <1613364605@qq.com>
2025-01-23 22:23:24 +0800

    Initial commit

---
+++ src/Action.java
@@ -0,0 +1,11 @@
+import ui.GameJFrame;
+import ui.LoginJFrame;
+import ui.RegisterJFrame;
+
+public class Action {
+    public static void main(String[] args) {
+        new LoginJFrame();
+        new RegisterJFrame();
+        new GameJFrame();
+    }
+}
